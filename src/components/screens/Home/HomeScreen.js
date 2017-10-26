270a986eb604ff34193cb404bfd94398ec95ed67
cornelius <cornelius.suermann@gmail.com>
2017-10-26 16:38:33 +0200

    add swiper

--- src/components/screens/Home/HomeScreen.js
+++ src/components/screens/Home/HomeScreen.js
@@ -1,11 +1,56 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import Swiper from 'react-native-swiper'
 
-export default class HomeScreen extends React.Component {
+export default class HomeScreen extends Component {
   static navigationOptions = {
     title: 'Welcome'
   }
   render () {
-    return <View><Text>Hello, Navigation!</Text></View>
+    return (
+      <Swiper
+        style={styles.wrapper}
+        showsButtons
+        loop={false}
+        onIndexChanged={index => console.log(`new index: ${index}`)}
+      >
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: {},
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB'
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5'
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
