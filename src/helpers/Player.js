590494f08faba4408679d36c1ca00728ff1b4a88
cornelius <cornelius.suermann@gmail.com>
2017-11-10 14:46:57 +0100

    play 1st episode

---
+++ src/helpers/Player.js
@@ -0,0 +1,45 @@
+import { Audio } from 'expo'
+
+export default class Player {
+  constructor () {
+    Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
+    })
+
+    this.initialStatus = {
+      shouldPlay: false,
+      rate: 1.0,
+      shouldCorrectPitch: true,
+      volume: 1.0,
+      isMuted: false,
+      isLooping: false
+    }
+
+    this.playbackInstance = null
+  }
+
+  async load (source) {
+    const { sound, status } = await Audio.Sound.create(
+      { uri: source },
+      this.initialStatus,
+      null // onPlaybackStatusUpdate
+    )
+    this.playbackInstance = sound
+  }
+
+  play () {
+    this.playbackInstance.playAsync()
+  }
+
+  pause () {
+    this.playbackInstance.pauseAsync()
+  }
+
+  stop () {
+    this.playbackInstance.stopAsync()
+  }
+}
--- src/sagas.js
+++ src/sagas.js
@@ -9,9 +9,31 @@ import {
   all
 } from 'redux-saga/effects'
 import { PLAY_BUTTON } from './reducers/PlayerReducer'
+import Player from '../src/helpers/Player'
+
+const player = new Player()
+
+// selector helpers
+const getPodcast = state => {
+  return state.subscriptions[state.player.selectedRssUrl]
+}
+
+const getMode = state => state.player.mode
 
 function * handlePlayButton (action) {
   console.log('PLAY was clicked!!!')
+
+  const currentMode = yield select(getMode)
+
+  if (currentMode === 'play') {
+    const podcast = yield select(getPodcast)
+    const episodeUrl = podcast.episodes[0].enclosure.url
+
+    yield player.load(episodeUrl)
+    player.play()
+  } else if (currentMode === 'pause') {
+    player.pause()
+  }
 }
 
 function * watchPlayButton () {
